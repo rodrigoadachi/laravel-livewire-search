aad744c7e45042e7b68416747fa3f87330477fa7
Rodrigo Adachi <adachi.rodrigo@gmail.com>
2025-03-15 17:04:48 -0300

    chore: add live option in  input component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,6 +3,7 @@
   'name',
   'placeholder' => '',
   'model' => null,
+  'live' => false,
   'disabled' => false
 ])
 
@@ -11,10 +12,17 @@
     type="{{ $type }}"
     name="{{ $name }}"
     placeholder="{{ $placeholder }}"
-    wire:model="{{ $model }}"
+    @if($model)
+      @if($live)
+        wire:model.live="{{ $model }}"
+      @else
+        wire:model="{{ $model }}"
+      @endif
+    @endif
     class="text-zinc-200 w-full h-10 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500
       @error($name) border-red-500 @enderror"
     style="background-color: #18181b;"
+    @if($disabled) disabled @endif
   />
   @error($name)
     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
